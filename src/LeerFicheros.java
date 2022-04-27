37cf77dd554c054472f8500190636b05775600d3
pcornejo <pedro.cornejo@iesnervion.es>
2022-04-27 14:37:05 +0200

    ejercicio acabado

--- src/LeerFicheros.java
+++ src/LeerFicheros.java
@@ -7,7 +7,7 @@ public class LeerFicheros {
     static final String FILE_NAME2 = "fichero2.dat";
     static final String FILE_NAME3 = "fichero3.dat";
 
-    static int[] tabla = new int[10];
+    static int[] tabla1 = new int[10];
     static int[] tabla2 = new int[10];
     static List<Integer> tabla3 = new ArrayList<>();
 
@@ -26,35 +26,34 @@ public class LeerFicheros {
             numero = Entrada1.read();
             while (numero != -1) {
                 //System.out.println(numero);
-                tabla[numero] = numero;
+                tabla1[numero] = numero;
                 numero = Entrada1.read();
             }
             tabla2 = (int[]) Entrada2.readObject();
             tabla3 = (List<Integer>) Entrada3.readObject();
             System.out.print("El resultado de la tabla numero 1 es: ");
-            for (int j : tabla) {
+            for (int j : tabla1) {
                 System.out.print(" " + j);
             }
-            System.out.println();
-            System.out.print("El resultado de la tabla numero 2 es: ");
+
+            System.out.print("\nEl resultado de la tabla numero 2 es: ");
             for (int j : tabla2) {
                 System.out.print(" " + j);
             }
-            System.out.println();
-            System.out.println("El resultado de la tabla numero 3 es: " + tabla3.toString());
+
+            System.out.println("\nEl resultado de la tabla numero 3 es: " + tabla3.toString());
         } catch (EOFException eofException) {
-            System.out.println("Final del fichero");
-            System.out.println("vamos a ver qué tiene la tabla");
+            System.out.println("Fin");
+            System.out.println("Tabla");
         } catch (IOException ioException) {
-            System.out.println("error en la apertura del fichero");
+            System.out.println("No se puede abrir el fichero");
         } catch (Exception exception) {
-            System.out.println("error general");
+            System.out.println("Error 404");
         } finally {
             //cerramos los flujos
             cerrarFlujos(Entrada1, Entrada2, Entrada3);
         }
     }
-
     static void cerrarFlujos(ObjectInputStream Entrada1,ObjectInputStream Entrada2,ObjectInputStream Entrada3){
         try {
             if (Entrada1 != null) {
